96a1970aad954c034ac50ded2979a1896e1ac278
Zaky fetoh <zaky.fetoh@gmail.com>
2023-02-03 23:50:55 +0200

    traditional SecTion isaddedto the related

--- Tables/to_latex.py
+++ Tables/to_latex.py
@@ -1,9 +1,9 @@
 import pandas as pd
   
-df = pd.read_excel("./Tables/reltx.xlsx")
+df = pd.read_excel("Tables/modifiedMBl.xlsx")
 
 rltx = df.to_latex(index=False)
 
-f = open("Tables/relatedlatex111.tex","w")
+f = open("Tables/modifMoble.tex","w")
 f.write(rltx)
 f.close(); 
